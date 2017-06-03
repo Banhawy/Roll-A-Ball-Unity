cc9b0ad134e4fba983b19b89d6cd67bea8b368db
Banhawy <the.benhawy@gmail.com>
2017-06-02 21:50:52 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		//Find and store refrence to PlayerObject Rigidbody component
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Called before any physics calculation. Physics code goes here
+	void FixedUpdate (){
+		//Grabs input from player through keyboard
+		//Variables record Horizontal and Vertical values which are controlled
+		//by keys on the keyboard
+		//We'll use this input to add forces to the rigid body and move the player
+		//Game object in the scene
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public int speed;
+	// Update is called once per frame
+	void Update () {
+		//Rotates Cubes. Multiply by deltaTime to be smooth and framerate independent 
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+	}
+}
